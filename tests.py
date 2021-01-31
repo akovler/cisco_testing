eac7bb0c1f1d99bdd798c9eeb14b89262e4558c9
Anna <akovler@gmail.com>
2021-01-30 20:10:24 -0800

    add venv

---
+++ tests.py
@@ -0,0 +1,122 @@
+import  logging
+import requests,sys
+import psutil # to kill proces with subprocesses
+from subprocess import Popen
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename="testwebservice.log",
+                    filemode='w')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+#output_file_handler = logging.FileHandler("testwebservice.log")
+stdout_handler = logging.StreamHandler(sys.stdout)
+
+#logger.addHandler(output_file_handler)
+logger.addHandler(stdout_handler)
+
+# Allows to kill process tree
+def kill_proc_tree(pid, including_parent=True):
+    parent = psutil.Process(pid)
+    children = parent.children(recursive=True)
+    for child in children:
+        child.kill()
+    gone, still_alive = psutil.wait_procs(children, timeout=5)
+    if including_parent:
+        parent.kill()
+        parent.wait(5)
+
+def test_put(key,value):
+    tmpdict={}
+    tmpdict['key'] = key
+    tmpdict['value'] = value
+    rc = requests.put(base_url, json=tmpdict)
+    if rc.ok:
+        print(rc.json())
+        logger.info("Success put item={}\n".format(rc.json()))
+    else:
+        logger.error("Error get request with parameter item={0}, rc= {1}\n".format(tmpdict, rc.status_code))
+        exit()
+    rc = requests.get(base_url + '//' + tmpdict['key'])
+    if rc.ok:
+        print(rc.json())
+        logger.info("Success get item={}(for put request)\n".format(rc.json()))
+    else:
+        logger.error(
+            "Error get request with parameter item={0}(for put request), rc= {1}\n".format(tmpdict, rc.status_code))
+        exit()
+
+def test_delete(key):
+    rc = requests.delete(base_url + '/' + key)
+    if rc.ok:
+        print(rc.json())
+        logger.info("Success delete item={}\n".format(rc.json()))
+    else:
+        logger.error("Error deltete item={0}, rc= {1}\n".format(tmpdict, rc.status_code))
+        # res.close()
+        exit()
+    rc = requests.get(base_url + '//' + tmpdict['key'])
+    if rc.ok:
+        print(rc.json())
+        logger.info("Success get item={}(testing 'delete')\n".format(rc.json()))
+    else:
+        logger.info(
+            "Error get request with parameter(testing 'delete') item={0}, rc= {1}\n".format(tmpdict, rc.status_code))
+
+
+try:
+    postdict=[{"key":"foo","value":"bar"},{"key":"foo1","value":"bar1"},{"key":"foo","value":"bar"}]
+
+    base_url='http://localhost:5000/kvstore'
+    try:
+        res = Popen([sys.executable,"webservice.py"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+    except Exception as e:
+        logger.critical(e, exc_info=True)
+
+    #1. testing 'post' request
+    for item in postdict:
+        rc = requests.post(base_url, json=item)
+        if rc.ok:
+            print (rc.json())
+            logger.info("Success post item={}\n".format(rc.json()))
+        else:
+            if rc.status_code !=409:
+                logger.error("Error post item={0}, rc= {1}\n".format(item,rc.status_code))
+                exit()
+            else:
+                logger.info("409 rc for post item={}\n".format(item))
+    #2. testing 'get all' request
+    rc = requests.get(base_url)
+    if rc.ok:
+        print(rc.json())
+        logger.info("Success get all items={}\n".format(rc.json()))
+    else:
+        logger.error("Error get all items={0}, rc= {1}\n".format(item,rc.status_code))
+        exit()
+
+    #3. testing 'get with parameter' request
+    for item in postdict:
+        rc = requests.get(base_url+'/'+item['key'])
+        if rc.ok:
+            print (rc.json())
+            logger.info("Success get item={}\n".format(rc.json()))
+        else:
+            logger.error("Error get request with parameter item={0}, rc= {1}\n".format(item, rc.status_code))
+            exit()
+    #4. testing 'put' request
+    test_put(postdict[0]['key'],'kuku')#existing key
+    test_put('somekey', 'somevalue')#arbitrary key,value
+    #5. testing 'delete' request
+    tmpdict = postdict[0]
+    test_delete(tmpdict['key']) #existing key
+    test_delete('kuku')#arbitrary key
+
+    print ("test ended")
+finally:
+    logger.info("Terminating FLASK application")
+    kill_proc_tree(res.pid, including_parent=False)
+    res.terminate()
+
---
+++ webservice.py
@@ -0,0 +1,90 @@
+from flask import Flask, request, Response, json
+app = Flask(__name__)
+mydict={}
+@app.route('/kvstore',methods=['POST'])
+
+def post_route():
+    data = request.get_data()
+    s = data.decode("utf-8")
+    pl = json.loads(s)
+    key=pl["key"]
+    val=pl["value"]
+    if key in mydict:
+        return Response(status=409)
+    mydict[key]=val
+    response = app.response_class(
+        response=json.dumps(pl),
+        status=201,
+        mimetype='application/json'
+    )
+    return response
+# curl -X POST localhost:5000/kvstore -d "{\"key\":\"foo\",\"value\":\"bar\"}" -H 'Content-Type:application/json'
+
+@app.route('/kvstore/<string:key>', methods=['GET'])
+def get_route(key):
+    tmpdict={}
+    if key in mydict:
+        val=mydict[key]
+        tmpdict["key"]=key
+        tmpdict["value"]=val
+        response = app.response_class(
+            response=json.dumps(tmpdict),
+            status=200,
+            mimetype='application/json')
+        return response
+
+    return Response(status=404)
+#curl -X GET localhost:5000/kvstore/foo
+
+@app.route('/kvstore', methods=['GET'])
+def getall_route():
+   response = app.response_class(
+       response=json.dumps(mydict),
+       status=200,
+       mimetype='application/json')
+   return response
+#curl -X GET localhost:5000/kvstore
+
+@app.route('/kvstore',methods=['PUT'])
+def put_route():
+    data = request.get_data()
+    # print('Data Received: "{data}"'.format(data=data))
+    s = data.decode("utf-8")
+    pl = json.loads(s)
+    key = pl["key"]
+    val = pl["value"]
+    if key not in mydict:
+        return Response(status=404)
+    mydict[key]=val
+    response = app.response_class(
+        response=json.dumps(pl),
+        status=200,
+        mimetype='application/json')
+    return response
+# curl -X PUT localhost:5000/kvstore -d "{\"key\":\"foo\",\"value\":\"kuku\"}" -H 'Content-Type:application/json'
+
+
+@app.route('/kvstore/<string:key>', methods=['DELETE'])
+def delete_route(key):
+    tmpdict={}
+    if key in mydict:
+        val=mydict[key]
+        tmpdict["key"]=key
+        tmpdict["value"]=val
+        mydict.pop(key)
+        response = app.response_class(
+            response=json.dumps(tmpdict),
+            status=200,
+            mimetype='application/json')
+        return response
+
+    return Response(status=404)
+#curl -X DELETE localhost:5000/kvstore/foo
+
+@app.route('/kvstore', methods=['HEAD','CONNECT','OPTIONS','TRACE','PATCH' ])
+def getother_route():
+   return Response(status=405)
+#curl -X CONNECT  -I -w "%{http_code}" localhost:5000/kvstore
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
